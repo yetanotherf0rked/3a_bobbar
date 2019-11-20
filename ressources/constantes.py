2cc5503fe3c790a706a1624d55525cd6314ce0a6
Alexandre <alexandre.gault@insa-cvl.fr>
2019-11-20 23:49:19 +0100

    Ajout d'une variable pour la mutation de la masse lors de la reproduction

--- ressources/constantes.py
+++ ressources/constantes.py
@@ -22,6 +22,8 @@ ENERGY_MOTHER = 50
 ENERGY_SON = 50
 #Taux de mutation de la vitesse
 MUT_VELOCITY = 0.1
+#Taux de mutation de la masse
+MUT_MASSE = 0.1
 
 # Chargement des images
 image_SOL = "ressources/images/isometric_pixel_0046.png"
