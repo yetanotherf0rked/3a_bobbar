3828ca7610118a0ace1edc02ea65d01a1b85e08d
Alexandre GAULT <alexandre.gault@insa-cvl.fr>
2019-11-22 11:05:33 +0100

    ajout de commentaires

--- model/bob.py
+++ model/bob.py
@@ -16,7 +16,7 @@ class Bob:
     def move_preference(self, grille):
         if self.perception < 1:
             return choice([(-1, 0), (1, 0), (0, -1), (0, 1)])
-        radius = int(self.perception)
+        radius = int(self.perception)  # on arrondi la perception à l'entier inférieur
         danger = False  # si il n'y a pas de danger on chasse, sinon on fuit
         max_food = 0
         largest_masse = 0
@@ -24,12 +24,12 @@ class Bob:
         for dx, dy in [(i, j) for i in range(-radius, radius+1) for j in range(abs(i)-radius, radius+1-abs(i))]:  # génère toutes les couples (dx, dy) dans un cercle de norme radius en distance euclidienne et de centre (0, 0)
             if 0 <= self.x + dx < TAILLE and 0 <= self.y+dy < TAILLE:  # si la position qu'on regarde est bien dans la grille
                 for other in grille[self.x+dx][self.y+dy].place:  # si il y a des bobs sur cette case
-                    if self.masse/other.masse < 2/3:
+                    if self.masse/other.masse < 2/3:  # si il y a un bob menaçant, on est en danger + update de la valeur de largest_masse
                         danger = True
                         if other.masse > largest_masse:
                             target = (self.x + dx, self.y + dy)
                             largest_masse = other.masse
-                    if (not danger) and other.masse/self.masse < 2/3:
+                    if (not danger) and other.masse/self.masse < 2/3:  # si on est en danger, on ne s'interesse plus à la chasse
                         potential_energy = 0.5*other.energy*(1-(other.masse/self.masse))
                         if potential_energy > max_food:
                             target = (self.x + dx, self.y + dy)
@@ -41,8 +41,8 @@ class Bob:
         if not max_food and not largest_masse:  # si aucune food et aucun bob menaçant n'a été trouvé
             return choice([(-1, 0), (1, 0), (0, -1), (0, 1)])
         if danger:
-            return max([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))
-        return min([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))
+            return max([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))  # retourne le (dx, dy) pour maximiser la distance avec le danger
+        return min([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))  # retourne le (dx, dy) pour minimiser la distance avec le gain
 
 
 
