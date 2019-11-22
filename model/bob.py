9bdef7409c7c8f441909fe1aff27ee904111a235
lmonnier <louis.monnier@insa-cvl.fr>
2019-11-22 11:35:00 +0100

    correction merge

--- model/bob.py
+++ model/bob.py
@@ -10,48 +10,12 @@ class Bob:
         self.energy = ENERGY_SPAWN
         self.velocity = 1.0
         self.masse = 1.0
-        self.memory_points = 0
-        self.energy_move = self.velocity**2*self.masse + self.memory_points/5
-        self.speed_buffer = 0.0
         self.perception = 0.0
+        self.memory_points = 0
+        self.energy_move = self.velocity**2*self.masse + self.perception/5 + self.memory_points/5
+        self.speed_buffer = 0.0
         self.memory = ([],[])# memory[0] -> cases deja visités  memory [1]-> food vu mais pas prio.
-
-    def move_preference(self, grille):
-        if self.perception < 1:
-            return choice([(-1, 0), (1, 0), (0, -1), (0, 1)])
-        radius = int(self.perception)  # on arrondi la perception à l'entier inférieur
-        danger = False  # si il n'y a pas de danger on chasse, sinon on fuit
-        max_food = 0
-        largest_masse = 0
-        target = (0, 0)
-        for dx, dy in [(i, j) for i in range(-radius, radius+1) for j in range(abs(i)-radius, radius+1-abs(i))]:  # génère toutes les couples (dx, dy) dans un cercle de norme radius en distance euclidienne et de centre (0, 0)
-            if 0 <= self.x + dx < TAILLE and 0 <= self.y+dy < TAILLE:  # si la position qu'on regarde est bien dans la grille
-                for other in grille[self.x+dx][self.y+dy].place:  # si il y a des bobs sur cette case
-                    if self.masse/other.masse < 2/3:  # si il y a un bob menaçant, on est en danger + update de la valeur de largest_masse
-                        danger = True
-                        if other.masse > largest_masse:
-                            target = (self.x + dx, self.y + dy)
-                            largest_masse = other.masse
-                    if (not danger) and other.masse/self.masse < 2/3:  # si on est en danger, on ne s'interesse plus à la chasse
-                        potential_energy = 0.5*other.energy*(1-(other.masse/self.masse))
-                        if potential_energy > max_food:
-                            target = (self.x + dx, self.y + dy)
-                            max_food = potential_energy
-                if not danger and grille[self.x+dx][self.y+dy].food > max_food:  # si on est en danger, on ne s'interesse plus à la food
-                    target = (self.x + dx, self.y + dy)
-                    max_food = grille[self.x+dx][self.y+dy].food
-
-        if not max_food and not largest_masse:  # si aucune food et aucun bob menaçant n'a été trouvé
-            return choice([(-1, 0), (1, 0), (0, -1), (0, 1)])
-        if danger:
-            return max([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))  # retourne le (dx, dy) pour maximiser la distance avec le danger
-        return min([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))  # retourne le (dx, dy) pour minimiser la distance avec le gain
-
-
-
-        
-        
-
+    
     def move(self, grille, dx, dy):
         nx=self.x+dx
         ny=self.y+dy
@@ -98,6 +62,38 @@ class Bob:
             #Ajout du fils dans la liste des Bobs et sur la grille
             listebob.insert(0,son)
             grille[self.x][self.y].place.append(son)
+
+    def move_preference(self, grille):
+        if self.perception < 1:
+            return choice([(-1, 0), (1, 0), (0, -1), (0, 1)])
+        radius = int(self.perception)  # on arrondi la perception à l'entier inférieur
+        danger = False  # si il n'y a pas de danger on chasse, sinon on fuit
+        max_food = 0
+        largest_masse = 0
+        target = (0, 0)
+        for dx, dy in [(i, j) for i in range(-radius, radius+1) for j in range(abs(i)-radius, radius+1-abs(i))]:  # génère toutes les couples (dx, dy) dans un cercle de norme radius en distance euclidienne et de centre (0, 0)
+            if 0 <= self.x + dx < TAILLE and 0 <= self.y+dy < TAILLE:  # si la position qu'on regarde est bien dans la grille
+                for other in grille[self.x+dx][self.y+dy].place:  # si il y a des bobs sur cette case
+                    if self.masse/other.masse < 2/3:  # si il y a un bob menaçant, on est en danger + update de la valeur de largest_masse
+                        danger = True
+                        if other.masse > largest_masse:
+                            target = (self.x + dx, self.y + dy)
+                            largest_masse = other.masse
+                    if (not danger) and other.masse/self.masse < 2/3:  # si on est en danger, on ne s'interesse plus à la chasse
+                        potential_energy = 0.5*other.energy*(1-(other.masse/self.masse))
+                        if potential_energy > max_food:
+                            target = (self.x + dx, self.y + dy)
+                            max_food = potential_energy
+                if not danger and grille[self.x+dx][self.y+dy].food > max_food:  # si on est en danger, on ne s'interesse plus à la food
+                    target = (self.x + dx, self.y + dy)
+                    max_food = grille[self.x+dx][self.y+dy].food
+
+        if not max_food and not largest_masse:  # si aucune food et aucun bob menaçant n'a été trouvé
+            return choice([(-1, 0), (1, 0), (0, -1), (0, 1)])
+        if danger:
+            return max([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))  # retourne le (dx, dy) pour maximiser la distance avec le danger
+        return min([(-1, 0), (1, 0), (0, -1), (0, 1)], key=lambda x: abs(target[0] - x[0]) + abs(target[1] - x[1]))  # retourne le (dx, dy) pour minimiser la distance avec le gain
+
 
     def save(self, case, mode=0):
         """ le bob stocke une case dans sa memoire
