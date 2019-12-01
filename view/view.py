b3dfdcc45c82721758182bc0b4cd3f15dddb2207
rankangi <matiou.bann@gmail.com>
2019-12-01 16:52:02 +0100

    - Ajout du resize de fenêtre à l'origine.

--- view/view.py
+++ view/view.py
@@ -16,7 +16,7 @@ class View:
         # Ouverture de la fenêtre Pygame
         self.fenetre = pygame.display.set_mode((960 * 2, 540 * 2))
         fond = pygame.image.load(image_FOND).convert_alpha()
-        self.fond = pygame.transform.scale(fond,(1920,1080))
+        self.fond = pygame.transform.scale(fond,(self.width,self.height))
 
         # Chargement et collage du fond
         # self.sol = pygame.image.load(image_SOL).convert_alpha()
@@ -44,13 +44,13 @@ class View:
                 # self.fenetre.blit(self.sol, (930 + x * 21 - 21 * y,20 + y * 12 + x * 12))
                 eval("self.fenetre.blit(self.terre" +str(self.grilleFond[x][y])+", (930 + " +str(x)+" * 18 - 18 * "+str(y)+", 8 + "+str(y)+"* 13.7 + "+str(x)+" * 13.7))")
                 if not(grille[x][y].food ==0):
-                    self.fenetre.blit(self.food, (930 + x * 18 - 18 * y,-5 + y * 13.7 + x * 13.7))
+                    self.fenetre.blit(self.food, (int(self.width/2)-30 + x * 18 - 18 * y,-5 + y * 13.7 + x * 13.7))
         # self.fenetre.blit(self.grass, (935 + 0 * 21 - 21 * 0, 500 + 8 + 0 * 12 + 0 * 12))
         # Affichage des Bobs
         for bob in listebob:
             x, y = bob.i, bob.j
             perso = pygame.transform.scale(self.perso , (32,int(8*bob.energy**(1/3))))
-            self.fenetre.blit(perso, (934 + x * 18 - 18 * y,2 + y * 13.7 + x * 13.7))
+            self.fenetre.blit(perso, (int(self.width/2)-26 + x * 18 - 18 * y,2 + y * 13.7 + x * 13.7))
         # Update
         pygame.display.flip()
         self.run = False
