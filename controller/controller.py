0a7fbe9546b0bdb9c80557f35eca2fe505d15f0c
lmonnier <louis.monnier@insa-cvl.fr>
2019-12-05 16:09:19 +0100

    2 run differents pour affichage et debug + passage du mode en argument au moment de l'execution de main.py

--- controller/controller.py
+++ controller/controller.py
@@ -10,21 +10,22 @@ from time import sleep
 import os
 
 
-AFFICHAGE = False
+#AFFICHAGE = False
 
 
 class Controller:
 
-    def __init__(self, simul=0):
+    def __init__(self, mode='a',simul=1000):
         # Initialisation de la grille
         self.grille = [[Case(x, y) for y in range(TAILLE)] for x in range(TAILLE)]
         #Initialisation des Bobs
         self.listebob = self.initbob(self.grille)
-        if AFFICHAGE:
+        if mode == 'a':
             self.view = View()
-        if not simul :
             self.run()
-        else :
+        if mode == 'd' :
+            self.run_debug()
+        if mode == 's' :
             self.simul(simul)
 
     #Initialisation des Bobs
@@ -40,7 +41,7 @@ class Controller:
 
     #Spawn de food
     def spawnfood(self, grille):
-        for k in range(NB_FOOD):
+        for _ in range(NB_FOOD):
             x, y = randint(0, TAILLE-1), randint(0, TAILLE-1)
             grille[x][y].food += ENERGY_FOOD
 
@@ -58,7 +59,9 @@ class Controller:
 
             #Si le bob est mort on le retire
             if bob.is_dead() :
+                self.grille[bob.x][bob.y].place.remove(bob)
                 self.listebob.remove(bob)
+                
 
         #on ajoute les nouveaux nés dans la liste de bobs qui sera actualisé au prochain tick
         self.listebob += new_bobs
@@ -79,45 +82,66 @@ class Controller:
                     self.spawnfood(self.grille)
                     print(day, len(self.listebob))
                 tick += 1
-                drawStats(self.grille, self.listebob, tick)
+                #drawStats(self.grille, self.listebob, tick)
                 self.listebob.sort(key=lambda x: x.velocity, reverse=True)
                 self.update()
-                sleep(0.01)
-                os.system('cls' if os.name == 'nt' else 'clear')
+                
 
 
-            if AFFICHAGE:
-                # Update de la fenêtre
-                while self.view.run:
-                    # Limitation de vitesse de la boucle
-                    sleep(0.001)
-                self._thread = Thread(target=self.view.affichage, args=(self.grille, self.listebob))
-                self._thread.start()
+            
+            # Update de la fenêtre
+            while self.view.run:
+                # Limitation de vitesse de la boucle
+                sleep(0.001)
+            self._thread = Thread(target=self.view.affichage, args=(self.grille, self.listebob))
+            self._thread.start()
 
-                # Test de fin
-                for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
+            # Test de fin
+            for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
 
-                    # Stop
-                    if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:  # Si un de ces événements est de type QUIT
-                        continuer = False  # On arrête la boucle
+                # Stop
+                if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:  # Si un de ces événements est de type QUIT
+                    continuer = False  # On arrête la boucle
 
-                    # Pause
-                    if event.type == KEYDOWN and event.key == K_SPACE:
-                        wait = not wait
+                # Pause
+                if event.type == KEYDOWN and event.key == K_SPACE:
+                    wait = not wait
 
-                    # Test si on a resize la fenêtre
-                    if event.type == VIDEORESIZE:
-                        self.view.width,self.view.height = event.size
+                # Test si on a resize la fenêtre
+                if event.type == VIDEORESIZE:
+                    self.view.width,self.view.height = event.size
 
-                    # Permet le déplacement dans la fenêtre
-                    if event.type == KEYDOWN and (event.key == K_UP or event.key == K_z):
-                        self.view.depy -= DEP_STEP
-                    if event.type == KEYDOWN and (event.key == K_DOWN or event.key == K_s):
-                        self.view.depy += DEP_STEP
-                    if event.type == KEYDOWN and (event.key == K_LEFT or event.key == K_q):
-                        self.view.depx -= DEP_STEP
-                    if event.type == KEYDOWN and (event.key == K_RIGHT or event.key == K_d):
-                        self.view.depx += DEP_STEP
+                # Permet le déplacement dans la fenêtre
+                if event.type == KEYDOWN and (event.key == K_UP or event.key == K_z):
+                    self.view.depy -= DEP_STEP
+                if event.type == KEYDOWN and (event.key == K_DOWN or event.key == K_s):
+                    self.view.depy += DEP_STEP
+                if event.type == KEYDOWN and (event.key == K_LEFT or event.key == K_q):
+                    self.view.depx -= DEP_STEP
+                if event.type == KEYDOWN and (event.key == K_RIGHT or event.key == K_d):
+                    self.view.depx += DEP_STEP
+
+    def run_debug(self):
+        tick = 0
+        day = 0
+        continuer = True
+        while continuer and self.listebob:
+            
+            # Comptage des ticks/Days
+            if tick % TICK_DAY == 0 :
+            # Suppression de la nourritue restante
+                self.removefood(self.grille)
+                day += 1
+                # Spawn de la nouvelle food
+                self.spawnfood(self.grille)
+                print(day, len(self.listebob))
+            tick += 1
+            drawStats(self.grille, self.listebob, tick)
+            self.listebob.sort(key=lambda x: x.velocity, reverse=True)
+            self.update()
+            sleep(0.01)
+            os.system('cls' if os.name == 'nt' else 'clear')
+
 
     def simul(self, ticks):
         """simule un nombre de tick donné et affiche l'etat de la simulation."""
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
+import sys
 from controller import *
+
 
 # Affiche la fenêtre au centre
 os.environ["SDL_VIDEO_CENTERED"] = "1"
-controller = Controller()
+
+#execution sans argument -> affichage vue 
+# python main.py [option..]
+#  a : affichage
+#  d : debug
+#  s : simulation de n tour passsé à la suite
+if len(sys.argv)>1 :
+    controller = Controller(sys.argv[1], int(sys.argv[2]) if len(sys.argv)>2 else 1000)
+else :
+     controller = Controller()
