0c606fc6183915e5cefeb0e2606f665f77021d57
rankangi <matiou.bann@gmail.com>
2020-01-07 23:39:40 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from controller import *
+from view.Choice import *
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+
+# Affiche la fenêtre au centre
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+
+
+class Prems(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+    def normal(self):
+        Controller(simul=self.Day_Box.value(),bar = self.progressBar)
+
+    def barMax(self):
+        if self.progressBar.value() == 100:
+            self.close()
+
+app = QApplication(sys.argv)
+
+window = Prems()
+window.show()
+
+app.exec()
+# Controller()
+
+# #execution sans argument -> affichage vue
+# # python main.py [option..]
+# #  a : affichage
+# #  d : debug
+# #  s : simulation de n tour passsé à la suite pour stats
+# if len(sys.argv)>1 :
+#     controller = Controller(sys.argv[1], int(sys.argv[2]) if len(sys.argv)>2 else 1000)
+# else :
+#      controller = Controller()
