ed45f69245d58fee92c9540a771171444c839fd5
lmonnier <lmonnier56@gmail.com>
2020-01-16 22:05:45 +0100

    suppression des fonction plus utiliséees pour l'affichage en cmd + ajout d'une fonction pour récuperer les stats pour graphs.

--- view/debug.py
+++ view/debug.py
@@ -29,31 +29,6 @@ def total_food(grille):
     return s
 
 
-def print_bar(maxd, dat, size=50):
-    length = int(dat) * size // maxd
-    for _ in range(length):
-        print('█', end='')
-    for _ in range(size - length):
-        print(' ', end='')
-    print(f'{dat}/{maxd}')
-
-
-def drawStats(grille, liste_bobs, tick):
-    print(f'Jour : {tick // self.config.TICK_DAY}')
-    print_bar(self.config.TICK_DAY, tick % self.config.TICK_DAY)
-    print(f'population : {len(liste_bobs)}')
-    mstat = mass_stat(liste_bobs)
-    print(f'masse moyenne : {mstat[0]:.3f} masse max : {mstat[1]:.3f} masse min : {mstat[2]:.3f}')
-    vstat = velocity_stat(liste_bobs)
-    print(f'vitesse moyenne : {vstat[0]:.3f} vitesse max : {vstat[1]:.3f} vitesse min : {vstat[2]:.3f}')
-    pstat = perception_stat(liste_bobs)
-    print(f'perception moyenne : {pstat[0]:.3f} perception max : {pstat[1]:.3f} perception min : {pstat[2]:.3f}')
-    memstat = memory_stat(liste_bobs)
-    print(f'mémoire moyenne : {memstat[0]:.3f} mémoire max : {memstat[1]:.3f} mémoire min : {memstat[2]:.3f}')
-    print('food :')
-    print_bar(parameters.get("Food Number") * parameters.get("Food Energy"), total_food(grille))
-
-
 def init_stats():
     """initialise les noms des statistiques à afficher sur l'interface GUI
     Les deux premières valeurs sont obligatoirement Day et Tick
@@ -83,3 +58,15 @@ def update_stats(grille, liste_bobs, tick):
             perception_stat(liste_bobs),
             memory_stat(liste_bobs)
             ]
+
+def update_stats_graphs(grille, liste_bobs, tick):
+    """ retourne un dictionaire avec les statistiques pour l'affichage en Graph"""
+    return {'ticks':tick,
+            'days': tick / ressources.config.para.TICK_DAY,
+            'pop': len(liste_bobs),
+            'food': int(total_food(grille) / ressources.config.para.ENERGY_FOOD),
+            'mass': mass_stat(liste_bobs),
+            'velocity': velocity_stat(liste_bobs),
+            'perception': perception_stat(liste_bobs),
+            'memory': memory_stat(liste_bobs)
+            }
