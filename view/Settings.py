97d587f529dd83337b2e39b9b035db4af185f4ab
Alexandre <alexandre.gault@insa-cvl.fr>
2020-01-28 22:12:18 +0100

    modification des formules pour la cout du mouvement et du cerveau fonctionnelle

--- view/Settings.py
+++ view/Settings.py
@@ -2,6 +2,8 @@ from PyQt5 import QtWidgets, QtCore
 
 from view.ui_Settings import Ui_Settings
 
+from math import exp, log
+
 import ressources.config
 
 
@@ -29,6 +31,44 @@ class SettingsWindow(QtWidgets.QWidget, Ui_Settings):
         self.config.show_Bord_Case = self.show_Bord_Case.isChecked()
         self.config.show_Nature = self.show_Nature.isChecked()
         self.config.show_Food_ProgressBar = self.show_Food_ProgressBar.isChecked()
+
+
+        """
+        MOVE = 0
+        BRAIN = 1
+
+        add=lambda x,y: x+y
+        sou=lambda x,y: x-y
+        mul=lambda x,y: x+y
+        div=lambda x,y: x/y
+
+        def formule_consommation(type):
+            #data = [principale, formule0, c1, c2, c3, formule1, d1, d2, d3]
+            # c1 * f0(c2*b) ^ c3 {+, -, x, / en fonction de principale} d1 * f0(d2*b) ^ d3 
+            
+            if type == MOVE:
+                p0, f0, c1, c2, c3, f1, d1, d2, d3 = config.
+            elif type == BRAIN:
+                p0, f0, c1, c2, c3, f1, d1, d2, d3 = config.
+            else:
+                print("formule_consommation: error type")
+                return lambda x, y:0
+
+            return lambda x, y:p0((c1*f0(c2*x))**c3, (d1*f1(d2*y))**d3)"""
+        add = lambda x, y: x+y
+        sou = lambda x, y: x-y
+        mul = lambda x, y: x*y
+        div = lambda x, y: x/y
+
+        velocity_function = [lambda x,y: exp(x*y), lambda x,y:log(abs(x*y) + 1), pow][self.move_function0.currentIndex()]
+        velocity_transform = lambda x: self.move_coeff0.value()*velocity_function(x, self.move_coeff1.value())
+
+        main_operation = [add, sou, mul, div][self.move_function1.currentIndex()]
+
+        mass_function = [lambda x,y: exp(x*y), lambda x,y:log(abs(x*y) + 1), pow][self.move_function2.currentIndex()]
+        mass_transform = lambda x: self.move_coeff2.value()*mass_function(x, self.move_coeff3.value())
+
+        self.config.move_consommation = lambda velocity, mass: main_operation(velocity_transform(velocity), mass_transform(mass))
 
     def gshow(self):
         pass
