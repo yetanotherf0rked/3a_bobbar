12ce1500329ca00b0de9373cc7097d2977ddaf15
rankangi <matiou.bann@gmail.com>
2020-02-02 21:54:56 +0100

    Modification meteo

--- view/view.py
+++ view/view.py
@@ -80,13 +80,14 @@ class View:
         self.grid = world.grid
         self.listebob = world.listebob
         self.run = True
-        if world.weather=="Sun":
-
-            self.sun.blit = self.simu_surface.blit(self.sun.image,(self.dim_simu[0]/2,0) )
 
 
         # Initialisation of the view
         PosX_init, PosY_init, cote_x, cote_y, simu_x, xdec, ydec = self.init_view(tick)
+        if self.config.weather=="Sun":
+            print("bonjour")
+
+            self.sun.blit = self.simu_surface.blit(self.sun.image_sun,(self.dim_simu[0]/2,0) )
 
         # Update et affichage Soleil
         self.soleil.updateListeX(cote_x)
